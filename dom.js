d28d71b774e13c3d1bdbbdac21a14fbf5bd075e1
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-10-13 09:22:44 -0400

    g
    h

---
+++ dom.js
@@ -0,0 +1,19 @@
+const DOMSelectors = {
+    form: document.querySelector("#form")
+    
+} 
+
+
+
+
+// no refresh code
+/* 
+
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach(
+        (el)=> el.textContent = DOMSelectors.firstName.value
+    );
+});  */
