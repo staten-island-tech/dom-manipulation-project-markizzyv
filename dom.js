a1bab80eb5de9012e70faad34e5b7e67f5d52c39
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-10-27 09:22:52 -0400

    s
    s

--- dom.js
+++ dom.js
@@ -1,11 +1,42 @@
-const DOMSelectors = {
+const form = {
     form: document.querySelector("#form"),
-    gallery: document.querySelector("card"),
+    
 } 
+console.log(form)
 
+const DOMSelectors = {
+    form: document.querySelector("form"),
+    firstName: document.querySelector("first-name"),
+    h2s: document.querySelector("h2s")
+}
+
+
+/* 
 function remove(){
     document.querySelector
-}
+} */
+
+
+
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach(
+        (el)=> el.textContent = DOMSelectors.firstName.value
+    );
+});  
+ 
+
+
+/* const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".first-name"),
+    h2s: document.querySelectorAll("h2"),
+// grab the text input
+// grab ALL the h2s
+};
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
@@ -13,5 +44,6 @@ DOMSelectors.form.addEventListener("submit", function(event){
     DOMSelectors.h2s.forEach(
         (el)=> el.textContent = DOMSelectors.firstName.value
     );
+    // one equals for the element text content
 }); 
-
+ */
