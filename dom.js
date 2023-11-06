d88fe26e9e096772c082e43a6a701e3766cd5be2
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-11-06 09:49:09 -0500

    h
    g

--- dom.js
+++ dom.js
@@ -1,56 +1,81 @@
-const form = {
-    form: document.querySelector("#form"),
-    
-} 
-console.log(form)
-
-const DOMSelectors = {
-    form: document.querySelector("form"),
-    firstName: document.querySelector("first-name"),
-    h2s: document.querySelector("h2s")
-}
-
-
-/* 
-function remove(){
-    document.querySelector
-} */
-
-const gallery = function addCard () {
-   
-    document.querySelector.h2s("card-title")
-    
-
-}
 
 
 
-
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2s.forEach(
-        (el)=> el.textContent = DOMSelectors.firstName.value
-    );
-});  
  
 
-
-/* const DOMSelectors = {
+const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
+    imgs: document.querySelector(".card-img")
+
 // grab the text input
 // grab ALL the h2s
 };
-
+//no refresh
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstName.value);
     DOMSelectors.h2s.forEach(
         (el)=> el.textContent = DOMSelectors.firstName.value
     );
+  
+
+
+
+
+   /* event.preventDefault()
+    console.log(DOMSelectors.secondName.value); 
+    DOMSelectors.imgs.forEach(
+        (el) => el.textContent = DOM.secondName.value
+    ); 
+  */
     // one equals for the element text content
 }); 
- */
+ DOMSelectors.imgs.addEventListener("submit", function() {
+    DOMSelectors.imgs.forEach((imgs) => {
+        imgs.addEventListener("submit", function () {
+            (imgs) => imgs.innerHTML = DOMSelectors.secondName.value
+        });
+    });
+})
+  
+
+/* const addCard 
+const confirm
+const clearInput
+*/
+
+
+
+
+
+
+const addCard = document.getElementById('gallery');
+addCard.DOMSelectors.secondName = document.querySelectors('.card-img')
+addCard.DOMSelectors.h2s(
+    (DOMSelectors.firstName) = console.log(addCard) 
+);
+
+
+ const confirmCard = document.getElementById('btn2');
+
+confirmCard.addEventListener('click', function() {
+    modalContainer.style.display = flex; 
+
+ 
+});
+
+const removeCard = document.getElementbyID('btn1');
+
+removeCard.addEventListener('click', function(){
+    modalContainer.style.display = 'none';
+}); 
+ 
+ 
+
+
+
+
+
+
