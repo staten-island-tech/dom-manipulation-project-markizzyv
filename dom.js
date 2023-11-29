0dc731e04716dc0045de30ee52ec5f784cad0d6f
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-11-28 20:24:31 -0500

    s
    s

--- dom.js
+++ dom.js
@@ -12,14 +12,7 @@ const DOMSelectors = {
     removebtn: document.querySelectorAll(".btn")
 
 
-    //submitbtn: document.getElementById("submit"),
-   
-    //para: document.querySelector(".card-desc")
-
-// grab the text input
-// grab ALL the h2s
 };
-//no refresh
 
 
 function addCard () {
@@ -34,8 +27,8 @@ let inputDesc = DOMSelectors.para.value
        "afterbegin",
         `
         <div class ="card">
-        <h2 class ="card-title>${inputTitle}</h2>
-        <img src ="${inputImg}">
+        <h2 class ="card-title">${inputTitle}</h2>
+        <img src ="${inputImg}" alt ="" class = "card-img">
         <p class ="card-desc">${inputDesc}</p>
         <button class ="btn">REMOVE</button>
         </div>
@@ -52,6 +45,54 @@ let inputDesc = DOMSelectors.para.value
 
 DOMSelectors.form.addEventListener("submit", (event) => {
     event.preventDefault();
+<<<<<<< HEAD
+=======
+    console.log(DOMSelectors.firstName.value);
+<<<<<<< HEAD
+    DOMSelectors.h2s.forEach(
+        (el)=> el.textContent = DOMSelectors.firstName.value
+    );
+ });
+
+ DOMSelectors.para.addEventListener('click', function () {
+      let inputDesc = DOMSelectors.para.value
+    DOMSelectors.cardDesc.insertAdjacentHTML("beforebegin", 
+    `
+    <p>${inputDesc}</p>
+    `);
+
+
+}); 
+
+
+
+
+ DOMSelectors.imgs.addEventListener("submit", function(){
+    DOMSelectors.imgs.forEach((imgs)=> imgs.innerHTML = DOMSelectors.imgs.value
+    );
+});
+=======
+>>>>>>> 9f37062a919bb258643529eecd6d745c90a6efb2
+
+  
+
+
+
+
+    let inputTitle = DOMSelectors.firstName.value
+    let inputIMG = DOMSelectors.imgs.value
+    let inputDesc = DOMSelectors.para.value
+
+
+document.querySelector(".gallery").insertAdjacentHTML("afterbegin", 
+`
+<div class="card">
+<h2 class= "card-title">${inputTitle}</h2>
+<img src="${inputIMG}">
+<p class="card-desc">${inputDesc}</p>
+</div>
+`);
+>>>>>>> a0d55413bb7ee0508f298667dac7d51744e9b7ca
 
 addCard ();
 });
